6deec29cbf6d8f53b8447521c25c84f43fdc7416
KiryaN31 <126331864+KiryaN31@users.noreply.github.com>
2023-02-27 10:45:45 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="login.css">
+  <title>STUDENT PORTAL </title>
+</head>
+<body>
+    <section>
+	<?php
+	$f1=$_POST
+	?>
+        <div class="form-box">
+            <div class="form-value">
+				<header>STUDENT PORTAL</header><br>
+					<div class="name">
+						<p><b>STUDENT NAME</b></p>
+					</div>
+					<div class="snum">
+						<p><b>STUDENT NUMBER</b></p>
+					</div>
+					<div class="yearsec">
+						<p>YEAR & SECTION</p>
+					</div>
+					<br><br>
+                    
+                    <div class="info">
+                       <button> INFORMATION </button>
+                    </div>
+                    <div class="grades">
+                        <button> GRADES </button>
+					</div>
+                    <div class="sched">
+                        <button> SCHEDULE </button>
+                    </div>
+					<div class="acct">
+						<button> ACCOUNTS </button>
+					</div>
+                    <div class="req">
+                        <button> REQUEST </button>
+					</div>
+					<div class="semsched">
+						<button> SEMESTRAL CALENDAR </button>
+					</div>
+            </div>		
+        </div>
+		<div class="creditbox"
+		<div id="watermark">
+		<i>for techinal issues, <ion-icon name="mail-open"></ion-icon> email us at example@gmail.com</i>
+		</div>
+    </section>
+	 </div>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	
+ 
+</body>
+</html>
+
